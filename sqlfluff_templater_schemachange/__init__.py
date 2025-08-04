43d696c0163dce9dca06a3e243a3d10a185c1f37
Thomas MacKay <mackay.thomas.e@gmail.com>
2025-08-03 17:06:16 -0700

    fixed import error after file rename

--- sqlfluff_templater_schemachange/__init__.py
+++ sqlfluff_templater_schemachange/__init__.py
@@ -1,8 +1,20 @@
 """SQLFluff templater plugin for schemachange integration."""
 
+import importlib.util
+import os
+
 from sqlfluff.core.plugin import hookimpl
 
-from sqlfluff_templater_schemachange.templater import SchemachangeTemplater
+# Import the module with hyphens in filename
+spec = importlib.util.spec_from_file_location(
+    "sqlfluff_templater_schemachange",
+    os.path.join(os.path.dirname(__file__), "sqlfluff-templater-schemachange.py"),
+)
+if spec is None or spec.loader is None:
+    raise ImportError("Could not load sqlfluff-templater-schemachange module")
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
+SchemachangeTemplater = module.SchemachangeTemplater
 
 __version__ = "0.1.0"
 __all__ = ["SchemachangeTemplater"]
